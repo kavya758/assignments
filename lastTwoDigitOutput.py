a8f7f7c278334e00c6fea36087c0c7c60d7afcc7
kavya758 <85903729+kavya758@users.noreply.github.com>
2021-06-14 16:16:17 -0400

    Add files via upload

---
+++ lastTwoDigitOutput.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def mul(listu):
+    product = 1
+    for i in listu:
+        product = product * i
+    product = product % 100
+    #print (product)
+    if (len(str(product)) == 1):
+        
+        if (product == 0):
+            print ('00')
+        else:
+            print ('0'+ str(product))  
+    
+    else:
+        if ((str(product)) != '00'):
+            print (product)
+        else:
+            print ('0'+ str(product))
+
+
+# In[5]:
+
+
+lst = []
+n = int(input("enter number of elements: "))
+
+if (n > 0):
+    for i in range(0,n):
+            numbers = int(input("Enter number "+ str(i) +":"))
+            lst.append(numbers)
+    mul(lst)
+    
+else:
+    print ("Invalid")
+
+
+# In[ ]:
+
+
+
+
